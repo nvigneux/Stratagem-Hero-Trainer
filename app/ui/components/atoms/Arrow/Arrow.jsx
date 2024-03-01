ccd76dcdc1089bfa3365cf54664ecb95ca5529b8
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-03-01 22:36:23 +0100

    [*] arrow component

---
+++ app/ui/components/atoms/Arrow/Arrow.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+
+// Components
+import { Picto } from '../Picto/Picto';
+
+// Lib
+import cn from '../../../../lib/cn';
+
+// Styles
+import styles from './Arrow.module.css';
+
+function Arrow({ direction, active, error }) {
+  return (
+    <Picto
+      icon="arrow"
+      className={cn([
+        styles.icon,
+        styles[direction],
+        active && styles.active,
+        error && styles.error,
+      ])}
+    />
+  );
+}
+
+Arrow.propTypes = {
+  direction: PropTypes.oneOf(['left', 'right', 'up', 'down']).isRequired,
+  active: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+};
+
+Arrow.List = function ArrowList({ children }) {
+  return (
+    <div className={styles.list}>{children}</div>
+  );
+};
+Arrow.List.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Arrow;
