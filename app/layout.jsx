9319d35335b351ce9e599f2da64c1d2828b4d35a
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-03-03 13:31:35 +0100

    [*] test vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import localFont from 'next/font/local';
+import { Analytics } from '@vercel/analytics/react';
 
 import './globals.css';
 
@@ -29,7 +30,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={fsSinclair.className}>{children}</body>
+      <body className={fsSinclair.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
