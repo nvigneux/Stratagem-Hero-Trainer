5177af76bfe02cf917fc8b5cfb170ea30aeed098
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-03-13 09:33:21 +0100

    [*] create stratagem

--- app/stratagems-admin/components/CreateStratagemForm.jsx
+++ app/stratagems-admin/components/CreateStratagemForm.jsx
@@ -5,73 +5,71 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { useFormState } from 'react-dom';
 
+// Styles
+import styles from './StratagemForm.module.css';
+
 // Lib
 import { createStratagem } from '../../lib/actions';
+
+// Components
+import Button from '../../ui/components/atoms/Button/Button';
+import LabelInput from '../../ui/components/atoms/LabelInput/LabelInput';
 
 export default function Form({ categoryId }) {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createStratagem, initialState);
 
   return (
-    <form action={dispatch}>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Category Categories */}
-        <div className="mb-4">
-          <label htmlFor="name" className="mb-2 block text-sm font-medium">
-            Choose an name
-            <div className="relative mt-2 rounded-md">
-              <div className="relative">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  placeholder="Enter name"
-                  required
-                />
-              </div>
-            </div>
-          </label>
-          <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.name && state.errors.name.map((error) => (
-              <p className="mt-2 text-sm text-red-500" key={error}>
-                {error}
-              </p>
-            ))}
-          </div>
-        </div>
-        {/* Category Categories */}
-        <div className="mb-4">
-          <label htmlFor="code" className="mb-2 block text-sm font-medium">
-            Choose a code
-            <div className="relative mt-2 rounded-md">
-              <div className="relative">
-                <input
-                  id="code"
-                  name="code"
-                  type="text"
-                  placeholder="Enter name"
-                  required
-                />
-              </div>
-            </div>
-          </label>
-          <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.code && state.errors.code.map((error) => (
-              <p className="mt-2 text-sm text-red-500" key={error}>
-                {error}
-              </p>
-            ))}
-          </div>
+    <form action={dispatch} className={styles.form}>
+      <div className={styles.fieldset}>
+        <LabelInput label="Nom">
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Enter name"
+            required
+          />
+        </LabelInput>
+        {/* TODO ERROR */}
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.name && state.errors.name.map((error) => (
+            <p key={error}>
+              {error}
+            </p>
+          ))}
         </div>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
+
+      <div className={styles.fieldset}>
+
+        <LabelInput label="Code">
+          <input
+            id="code"
+            name="code"
+            type="text"
+            placeholder="Enter name"
+            required
+          />
+        </LabelInput>
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.code && state.errors.code.map((error) => (
+            <p key={error}>
+              {error}
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.actions}>
         <Link
-          href="/strateagems-admin"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+          href="/stratagems-admin"
         >
-          Cancel
+          <Button>
+            Cancel
+          </Button>
         </Link>
-        <button type="submit">Create Category</button>
+        <Button type="submit">Create Stratagem</Button>
       </div>
 
       <input type="hidden" name="category_id" value={categoryId} />
