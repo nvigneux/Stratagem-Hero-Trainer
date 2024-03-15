6014a685272d84cb211f75931711c7097a5fc96d
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-03-15 10:31:19 +0100

    [*] refacto delete button

--- app/stratagems-admin/components/DeleteCategory.jsx
+++ app/stratagems-admin/components/DeleteCategory.jsx
@@ -3,14 +3,17 @@ import PropTypes from 'prop-types';
 // Lib
 import { deleteCategory } from '../../lib/actions';
 
+// Components
+import Button from '../../ui/components/atoms/Button/Button';
+
 function DeleteCategory({ id }) {
   const deleteCategoryWithId = deleteCategory.bind(null, id);
 
   return (
     <form action={deleteCategoryWithId}>
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+      <Button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
-      </button>
+      </Button>
     </form>
   );
 }
--- app/stratagems-admin/components/DeleteStratagem.jsx
+++ app/stratagems-admin/components/DeleteStratagem.jsx
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 // Lib
 import { deleteStratagem } from '../../lib/actions';
 
+// Components
+import Button from '../../ui/components/atoms/Button/Button';
+
 function DeleteStratagem({ id, categoryId }) {
   const deleteStratagemWithId = deleteStratagem.bind(null, id);
 
   return (
     <form action={deleteStratagemWithId}>
       <input type="hidden" name="category_id" value={categoryId} />
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+      <Button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
-      </button>
+      </Button>
     </form>
   );
 }
