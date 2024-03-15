c1aa324ca7d4704e9dd6bf265e9544b696f6540c
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-03-15 10:30:46 +0100

    [*] Table page

--- app/stratagems-admin/[id]/page.jsx
+++ app/stratagems-admin/[id]/page.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
+import Image from 'next/image';
+import { Fragment } from 'react';
 
 // Lib
 import { fetchCategoryById, fetchStratagemByCategory } from '../../lib/data';
+
+// Components
 import DeleteStratagem from '../components/DeleteStratagem';
+import Button from '../../ui/components/atoms/Button/Button';
+import Table from '../../ui/components/atoms/Table/Table';
 
 export default async function Page({ params }) {
   const { id } = params;
@@ -17,19 +23,50 @@ export default async function Page({ params }) {
     notFound();
   }
 
+  const columns = [
+    { id: 'image', title: 'Image', width: '1px' },
+    { id: 'name', title: 'Name', width: 'auto' },
+    { id: 'actions', title: 'Actions', width: '1px' },
+  ];
+
   return (
     <main>
-      <Link href={`/stratagems-admin/${category.id}/create`}>Ajouter Stratagem</Link>
-      <Link href={`/stratagems-admin/${category.id}/edit`}>Edit</Link>
-      <Link href="/stratagems-admin">Retour</Link>
-      <h1>{category.name}</h1>
-      {stratagems.map((stratagem) => (
-        <div key={stratagem.id}>
-          <h2>{stratagem.name}</h2>
-          <Link href={`/stratagems-admin/${category.id}/${stratagem.id}`}>Edit</Link>
-          <DeleteStratagem id={stratagem.id} categoryId={category.id} />
-        </div>
-      ))}
+      <Table.Action>
+        <Link href="/stratagems-admin"><Button>Back</Button></Link>
+        <Link href={`/stratagems-admin/${category.id}/create`}>
+          <Button>Add Stratagem</Button>
+        </Link>
+      </Table.Action>
+
+      <Table>
+        <Table.Head columns={columns}>
+          {(column) => (
+            <Table.HeaderCell key={column.id} width={column.width}>
+              {column.title}
+            </Table.HeaderCell>
+          )}
+        </Table.Head>
+        <Table.Body columns={columns} data={stratagems}>
+          {(row) => (
+            <Fragment key={row.id}>
+              <Table.Cell>
+                <Image
+                  src={`/icons/stratagems/${category.name}/${row.name}.svg`}
+                  alt={row.name}
+                  width={35}
+                  height={35}
+                />
+              </Table.Cell>
+
+              <Table.Cell>{row.name}</Table.Cell>
+              <Table.Cell>
+                <Link href={`/stratagems-admin/${category.id}/${row.id}`}><Button>Edit</Button></Link>
+                <DeleteStratagem id={row.id} categoryId={category.id} />
+              </Table.Cell>
+            </Fragment>
+          )}
+        </Table.Body>
+      </Table>
     </main>
   );
 }
--- app/stratagems-admin/components/Categories.jsx
+++ app/stratagems-admin/components/Categories.jsx
@@ -1,28 +1,49 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 // Lib
+import { Fragment } from 'react';
 import { fetchFilteredCategories } from '../../lib/data';
-// import DeleteCategory from './DeleteCategory';
 
-async function Invoices({ query, currentPage, pageSize }) {
+// Components
+import Table from '../../ui/components/atoms/Table/Table';
+import Button from '../../ui/components/atoms/Button/Button';
+
+async function Categories({ query, currentPage, pageSize }) {
   const categories = await fetchFilteredCategories(query, currentPage, pageSize);
 
+  const columns = [
+    { id: 'name', title: 'Name', width: 'auto' },
+    { id: 'actions', title: 'Actions', width: '1px' },
+  ];
+
   return (
-    <div>
-      {categories.map((category) => (
-        <div key={category.id}>
-          <Link href={`/stratagems-admin/${category.id}`}>{category.name}</Link>
-          {/* <DeleteCategory id={category.id} /> */}
-        </div>
-      ))}
-    </div>
+    <Table>
+      <Table.Head columns={columns}>
+        {(column) => (
+          <Table.HeaderCell key={column.id} width={column.width}>
+            {column.title}
+          </Table.HeaderCell>
+        )}
+      </Table.Head>
+      <Table.Body columns={columns} data={categories}>
+        {(row) => (
+          <Fragment key={row.id}>
+            <Table.Cell>{row.name}</Table.Cell>
+            <Table.Cell>
+              <Link href={`/stratagems-admin/${row.id}`}><Button>Detail</Button></Link>
+              <Link href={`/stratagems-admin/${row.id}/edit`}><Button>Edit</Button></Link>
+            </Table.Cell>
+          </Fragment>
+        )}
+      </Table.Body>
+    </Table>
   );
 }
 
-Invoices.propTypes = {
+Categories.propTypes = {
   query: PropTypes.string.isRequired,
   currentPage: PropTypes.number.isRequired,
   pageSize: PropTypes.number.isRequired,
 };
 
-export default Invoices;
+export default Categories;
