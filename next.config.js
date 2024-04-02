8de2a0fa8f5992e1ce08d027898e9e8dfcbbbfec
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-04-02 13:08:58 +0200

    Config next to import sound

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     });
 
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            name: '[name]-[hash].[ext]',
+          },
+        },
+      ],
+    });
+
     return config;
   },
 };
