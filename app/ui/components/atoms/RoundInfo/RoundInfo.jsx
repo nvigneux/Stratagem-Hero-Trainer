903f56d60bf97a6bcdd41c9946453bc051419d0b
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-04-29 10:01:45 +0200

    Add Round info button history

--- app/ui/components/atoms/RoundInfo/RoundInfo.jsx
+++ app/ui/components/atoms/RoundInfo/RoundInfo.jsx
@@ -5,11 +5,14 @@ import styles from './RoundInfo.module.css';
 
 // Lib
 import cn from '../../../../lib/cn';
+import { Picto } from '../Picto/Picto';
 
-function RoundInfo({ roundNb, className }) {
+function RoundInfo({ roundNb, children, className }) {
   return (
     <div className={styles.container}>
-      <div className={styles.label}>Round</div>
+      <div className={styles.labelContainer}>
+        {children}
+      </div>
       <div className={cn([styles.round, className])}>{roundNb}</div>
     </div>
   );
@@ -17,7 +20,30 @@ function RoundInfo({ roundNb, className }) {
 
 RoundInfo.propTypes = {
   roundNb: PropTypes.number.isRequired,
+  children: PropTypes.node.isRequired,
   className: PropTypes.string.isRequired,
 };
 
 export default RoundInfo;
+
+export function RoundInfoButton({ onClick, disabled }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={styles.historyButton}
+      aria-label="Show history"
+      title="Round History & Stats"
+      disabled={disabled}
+    >
+      <div className={styles.label}>Round</div>
+      <div className={styles.historyIcon}>
+        <Picto icon="history" />
+      </div>
+    </button>
+  );
+}
+RoundInfoButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
