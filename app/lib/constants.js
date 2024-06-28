3daf3fd267c7d851735901245dde4edb7cc9dac1
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-06-28 13:32:56 +0200

    [*] contact link section

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -2,3 +2,5 @@
 export const COOKIE_BEST_SCORE = 'h2-hero-best_score';
 export const COOKIE_LOADOUT = 'h2-loadout';
 export const COOKIE_SETTINGS = 'h2-settings';
+
+export const CONTACT_LINK = 'https://github.com/nvigneux/Stratagem-Hero-Trainer/issues/new';
