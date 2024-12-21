24e898cd047dfa4e120e07a9853b11b1d114855b
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-22 00:28:00 +0100

    Enhance error and not-found components with JSDoc comments for better documentation; update middleware with detailed function description

--- app/(overview)/error.jsx
+++ app/(overview)/error.jsx
@@ -1,10 +1,14 @@
-/* eslint-disable react/require-default-props */
-
 'use client';
 
-import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 
+/**
+ * Error component
+ * @param {object} props - Component properties
+ * @param {Error} props.error - Error object
+ * @param {Function} props.reset - Function to reset the error state
+ * @returns {JSX.Element} The Error component
+ */
 function Error({ error, reset }) {
   useEffect(() => {
     // Optionally log the error to an error reporting service
@@ -16,7 +20,8 @@ function Error({ error, reset }) {
       <h2 className="text-center">Something went wrong!</h2>
       <button
         type="button"
-        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm
+        text-white transition-colors hover:bg-blue-400"
         onClick={
           // Attempt to recover by trying to re-render the invoices route
           () => reset()
@@ -27,10 +32,5 @@ function Error({ error, reset }) {
     </main>
   );
 }
-
-Error.propTypes = {
-  error: PropTypes.shape({}),
-  reset: PropTypes.func,
-};
 
 export default Error;
