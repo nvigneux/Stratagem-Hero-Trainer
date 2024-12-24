a451543037ac52974ff7a364da7df5d4d29e3846
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-24 01:46:13 +0100

    Refactor Cypress test for Stratagem Trainer to improve clarity and enhance gameplay flow validation

--- cypress/e2e/stratagems.cy.js
+++ cypress/e2e/stratagems.cy.js
@@ -1,11 +1,11 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Stratagem Trainer - Gameplay and Settings", () => {
+  it("Validates gameplay flow and settings adjustments", () => {
     cy.visit("https://stratagem-hero-trainer.vercel.app/");
-    /* ==== Generated with Cypress Studio ==== */
+
     cy.toggleCheckbox(
       ".StrategemsLayout_checkboxAll__ZLN7R > .Checkbox_checkbox-checkmark__kP1Gy",
       false
-    )
+    );
 
     // Check msg when no stratagem is selected
     cy.get(".StratagemsName_name__E4XEJ").should(
@@ -21,24 +21,33 @@ describe("template spec", () => {
     cy.performKeyCombination(["down", "left", "down", "up", "right"], 6);
 
     // Check if round is incremented
-    cy.get('.RoundInfo_round__WYuUI').should('have.text', '2');
-    cy.get(':nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ').should('have.text', '75');
-    cy.get(':nth-child(3) > .ScoreInfo_bonusValue__ZTzmJ').should('have.text', '100');
+    cy.get(".RoundInfo_round__WYuUI").should("have.text", "2");
+    cy.get(":nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ").should(
+      "have.text",
+      "75"
+    );
+    cy.get(":nth-child(3) > .ScoreInfo_bonusValue__ZTzmJ").should(
+      "have.text",
+      "100"
+    );
 
     // Change sound settings
-    cy.get('.StratagemsGame_buttonSettings__JIFMu').click();
-    cy.get('#gameSound').check();
-    cy.get(':nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn').click();
+    cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
+    cy.get("#gameSound").check();
+    cy.get(":nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn").click();
 
     // Change timer settings
-    cy.get('#timerDuration').clear();
-    cy.get('#timerDuration').type('1');
-    cy.get(':nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn').click();
+    cy.get("#timerDuration").clear();
+    cy.get("#timerDuration").type("1");
+    cy.get(":nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn").click();
 
     // click to close the settings
-    cy.get('.StratagemsGame_settingsOverlay__RuniU').click();
+    cy.get(".StratagemsGame_settingsOverlay__RuniU").click();
 
     // Check if game is loose
-    cy.get(':nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ').should('have.text', '0');
+    cy.get(":nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ").should(
+      "have.text",
+      "0"
+    );
   });
 });
