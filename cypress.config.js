20536e9ab0dfcde3f037704594f0d18a8dcd0452
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-24 01:45:08 +0100

    Enable experimental studio in Cypress configuration and enhance stratagems test with additional interactions and assertions

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    experimentalStudio: true,
     viewportWidth: 1440,
     viewportHeight: 800,
   },
