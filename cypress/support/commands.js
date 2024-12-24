31a204f602c82f815f77303e9c6a25e04288ea57
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-24 01:43:40 +0100

    Add stratagems commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,37 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * Check if a checkbox is checked or not
+ * @param {string} selector - The selector of the checkbox
+ * @param {boolean} shouldBeChecked - Whether the checkbox should be checked or not
+ */
+Cypress.Commands.add("toggleCheckbox", (selector, shouldBeChecked) => {
+  cy.get(selector).click();
+  const assertion = shouldBeChecked ? "be.checked" : "not.be.checked";
+  cy.get(selector).should(assertion);
+});
+
+/**
+ * Select a stratagem by name
+ * @param {string} stratagemName - The name of the stratagem to select
+ */
+Cypress.Commands.add("selectStratagem", (stratagemName) => {
+  cy.get(
+    `[for="${stratagemName}"] > .StratagemsCard_card__sdnRd > .StratagemsCard_icon__bngXX`
+  ).click();
+  cy.get(`[id="${stratagemName}"]`).check({ force: true });
+  cy.get(
+    ".StratagemsGameCard_active__UNhvJ > .StratagemsGameCard_icon__yPyqA"
+  ).should("have.attr", "alt", stratagemName);
+});
+
+/**
+ * Perform a series of key presses
+ * @param {string[]} keys - The keys to press
+ * @param {number} [times=1] - The number of times to press the keys
+ */
+Cypress.Commands.add("performKeyCombination", (keys, times = 1) => {
+  for (let i = 0; i < times; i++) {
+    keys.forEach((key) => {
+      cy.get("body").click().type(`{${key}arrow}`).wait(100);
+    });
+  }
+});
