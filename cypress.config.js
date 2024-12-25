7cc22fe52f1a4daa11924158a4935ee97d556486
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-25 18:32:34 +0100

    Fix EsLint cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
     experimentalStudio: true,
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-undef */
 /**
  * Check if a checkbox is checked or not
  * @param {string} selector - The selector of the checkbox
  * @param {boolean} shouldBeChecked - Whether the checkbox should be checked or not
  */
-Cypress.Commands.add("toggleCheckbox", (selector, shouldBeChecked) => {
+Cypress.Commands.add('toggleCheckbox', (selector, shouldBeChecked) => {
   cy.get(selector).click({ force: true });
-  const assertion = shouldBeChecked ? "be.checked" : "not.be.checked";
+  const assertion = shouldBeChecked ? 'be.checked' : 'not.be.checked';
   cy.get(selector).should(assertion);
 });
 
@@ -13,14 +14,14 @@ Cypress.Commands.add("toggleCheckbox", (selector, shouldBeChecked) => {
  * Select a stratagem by name
  * @param {string} stratagemName - The name of the stratagem to select
  */
-Cypress.Commands.add("selectStratagem", (stratagemName) => {
+Cypress.Commands.add('selectStratagem', (stratagemName) => {
   cy.get(
-    `[for="${stratagemName}"] > .StratagemsCard_card__sdnRd > .StratagemsCard_icon__bngXX`
+    `[for="${stratagemName}"] > .StratagemsCard_card__sdnRd > .StratagemsCard_icon__bngXX`,
   ).click();
   cy.get(`[id="${stratagemName}"]`).check({ force: true });
   cy.get(
-    ".StratagemsGameCard_active__UNhvJ > .StratagemsGameCard_icon__yPyqA"
-  ).should("have.attr", "alt", stratagemName);
+    '.StratagemsGameCard_active__UNhvJ > .StratagemsGameCard_icon__yPyqA',
+  ).should('have.attr', 'alt', stratagemName);
 });
 
 /**
@@ -28,27 +29,27 @@ Cypress.Commands.add("selectStratagem", (stratagemName) => {
  * @param {string[]} keys - The keys to press
  * @param {number} [times=1] - The number of times to press the keys
  */
-Cypress.Commands.add("performKeyCombination", (keys, times = 1) => {
-  for (let i = 0; i < times; i++) {
+Cypress.Commands.add('performKeyCombination', (keys, times = 1) => {
+  for (let i = 0; i < times; i += 1) {
     keys.forEach((key) => {
-      cy.get("body").click().type(`{${key}arrow}`).wait(100);
+      cy.get('body').click().type(`{${key}arrow}`).wait(100);
     });
   }
 });
 
 // set settings cookies by visiting the page and change input timerDuration
-Cypress.Commands.add("setSettings", (timerDuration) => {
-  cy.session("h2-settings", () => {
-    cy.visit("http://localhost:3000/");
+Cypress.Commands.add('setSettings', (timerDuration) => {
+  cy.session('h2-settings', () => {
+    cy.visit('http://localhost:3000/');
     // Change game sound settings
-    cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
+    cy.get('.StratagemsGame_buttonSettings__JIFMu').click();
     cy.wait(500);
-    cy.toggleCheckbox("#gameSound", true);
-    cy.get(":nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn").click();
+    cy.toggleCheckbox('#gameSound', true);
+    cy.get(':nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn').click();
 
     // Change timer settings
-    cy.get("#timerDuration").clear().type(timerDuration);
-    cy.get(":nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn").click();
+    cy.get('#timerDuration').clear().type(timerDuration);
+    cy.get(':nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn').click();
     cy.wait(500);
   });
 });
