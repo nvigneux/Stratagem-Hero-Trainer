c9281978901d22cc5181f652cd1203ca86376d76
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-25 17:47:53 +0100

    Refactor Stratagem Trainer tests to improve gameplay flow validation and settings persistence

--- cypress/e2e/stratagems.cy.js
+++ cypress/e2e/stratagems.cy.js
@@ -1,77 +1,63 @@
-// describe("Stratagem Trainer - Gameplay and Settings", () => {
-//   it("Validates gameplay flow and settings adjustments", () => {
-//     cy.visit("https://stratagem-hero-trainer.vercel.app/");
+describe("Stratagem Trainer - Gameplay and Settings", () => {
+  it("Validates gameplay flow and settings adjustments", () => {
+    cy.visit("https://stratagem-hero-trainer.vercel.app/");
 
-//     cy.toggleCheckbox(
-//       ".StrategemsLayout_checkboxAll__ZLN7R > .Checkbox_checkbox-checkmark__kP1Gy",
-//       false
-//     );
+    cy.toggleCheckbox(
+      ".StrategemsLayout_checkboxAll__ZLN7R > .Checkbox_checkbox-checkmark__kP1Gy",
+      false
+    );
 
-//     // Check msg when no stratagem is selected
-//     cy.get(".StratagemsName_name__E4XEJ").should(
-//       "have.text",
-//       "Traitor detected !"
-//     );
-//     cy.get(".RoundInfo_round__WYuUI").should("have.text", "1");
+    // Check msg when no stratagem is selected
+    cy.get(".StratagemsName_name__E4XEJ").should(
+      "have.text",
+      "Traitor detected !"
+    );
+    cy.get(".RoundInfo_round__WYuUI").should("have.text", "1");
 
-//     // Select a stratagem
-//     cy.selectStratagem("Machine Gun");
+    // Select a stratagem
+    cy.selectStratagem("Machine Gun");
 
-//     // Perform a key combination
-//     cy.performKeyCombination(["down", "left", "down", "up", "right"], 6);
+    // Perform a key combination
+    cy.performKeyCombination(["down", "left", "down", "up", "right"], 6);
 
-//     // Check if round is incremented
-//     cy.get(".RoundInfo_round__WYuUI").should("have.text", "2");
-//     cy.get(":nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ").should(
-//       "have.text",
-//       "75"
-//     );
-//     cy.get(":nth-child(3) > .ScoreInfo_bonusValue__ZTzmJ").should(
-//       "have.text",
-//       "100"
-//     );
+    // Check if round is incremented
+    cy.get(".RoundInfo_round__WYuUI").should("have.text", "2");
+    cy.get(":nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ").should(
+      "have.text",
+      "75"
+    );
+    cy.get(":nth-child(3) > .ScoreInfo_bonusValue__ZTzmJ").should(
+      "have.text",
+      "100"
+    );
 
-//     // Change sound settings
-//     cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
-//     cy.get("#gameSound").check();
-//     cy.get(":nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn").click();
+    // Change sound settings
+    cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
+    cy.get("#gameSound").check();
+    cy.get(":nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn").click();
 
-//     // Change timer settings
-//     cy.get("#timerDuration").clear();
-//     cy.get("#timerDuration").type("1");
-//     cy.get(":nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn").click();
+    // Change timer settings
+    cy.get("#timerDuration").clear();
+    cy.get("#timerDuration").type("5");
+    cy.get(":nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn").click();
 
-//     // click to close the settings
-//     cy.get(".StratagemsGame_settingsOverlay__RuniU").click();
+    // click to close the settings
+    cy.get(".StratagemsGame_settingsOverlay__RuniU").click();
 
-//     // Check if game is loose
-//     cy.get(":nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ").should(
-//       "have.text",
-//       "0"
-//     );
-//   });
-// });
+    // Check if game is loose
+    cy.get(":nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ").should(
+      "have.text",
+      "0"
+    );
+  });
 
-// describe("Settings Persistence", () => {
-//   it("Persists sound and timer settings", () => {
-//     cy.visit("https://stratagem-hero-trainer.vercel.app/");
+  it("Persists sound and timer settings after reload", () => {
+    cy.setSettings(5);
+    cy.visit("https://stratagem-hero-trainer.vercel.app/");
+    cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
+    cy.get("#timerDuration").should("have.value", "5");
+  });
 
-//     // Modifie les paramètres
-//     cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
-//     cy.get("#gameSound").check();
-//     cy.get("#timerDuration").clear().type("5");
-//     cy.get(":nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn").click();
-//     cy.get(".StratagemsGame_settingsOverlay__RuniU").click();
-
-//     // Recharge la page et vérifie les paramètres
-//     cy.reload();
-//     cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
-//     cy.get("#gameSound").should("be.checked");
-//     cy.get("#timerDuration").should("have.value", "5");
-//   });
-// });
-
-describe("Stratagem Selection", () => {
   it("Allows selecting and displaying stratagems", () => {
     cy.visit("https://stratagem-hero-trainer.vercel.app/");
 
@@ -80,14 +66,12 @@ describe("Stratagem Selection", () => {
       false
     );
 
-    // Sélectionne un stratagème
+    // Select a stratagem
     cy.selectStratagem("Machine Gun");
     cy.get(".StratagemsCard_active__rDaiJ").should("exist");
     cy.get(".StratagemsCard_active__rDaiJ").should("have.length", 1);
   });
-});
 
-describe("Key Combination Validation", () => {
   it("Handles successful and failed combinations", () => {
     cy.visit("https://stratagem-hero-trainer.vercel.app/");
 
@@ -96,15 +80,15 @@ describe("Key Combination Validation", () => {
       false
     );
 
-    // Sélectionne un stratagème
+    // Select a stratagem
     cy.selectStratagem("Machine Gun");
 
-    // Réalise une combinaison correcte
+    // Perform a correct combination
     cy.performKeyCombination(["down", "left", "down", "up", "right"], 1);
-    cy.get('.ScoreInfo_score__xU7Z2').should('have.text', '25');
-    
-    // Fait une erreur dans la combinaison
+    cy.get(".ScoreInfo_score__xU7Z2").should("have.text", "25");
+
+    // Make a mistake in the combination
     cy.performKeyCombination(["down", "left", "up", "up", "right"], 1);
-    cy.get('.ScoreInfo_score__xU7Z2').should('have.text', '25');
+    cy.get(".ScoreInfo_score__xU7Z2").should("have.text", "25");
   });
 });
