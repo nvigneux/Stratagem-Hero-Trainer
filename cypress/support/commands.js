145860a0c24a237de1db65c4dc746a0272d8fe76
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-25 18:24:26 +0100

    Refactor setSettings command to use local development URL and improve code clarity

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -39,20 +39,16 @@ Cypress.Commands.add("performKeyCombination", (keys, times = 1) => {
 // set settings cookies by visiting the page and change input timerDuration
 Cypress.Commands.add("setSettings", (timerDuration) => {
   cy.session("h2-settings", () => {
-    cy.visit("https://stratagem-hero-trainer.vercel.app/");
+    cy.visit("http://localhost:3000/");
     // Change game sound settings
     cy.get(".StratagemsGame_buttonSettings__JIFMu").click();
     cy.wait(500);
     cy.toggleCheckbox("#gameSound", true);
-    cy.get(
-      ":nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn"
-    ).click();
+    cy.get(":nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn").click();
 
     // Change timer settings
     cy.get("#timerDuration").clear().type(timerDuration);
-    cy.get(
-      ":nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn"
-    ).click();
+    cy.get(":nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn").click();
     cy.wait(500);
   });
 });
