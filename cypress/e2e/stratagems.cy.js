2f36432f1c4224db58bee20685ac17c4b4f09b31
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-12-28 00:41:39 +0100

    Refactor Cypress tests to use data-testid attributes for improved readability and maintainability

--- cypress/e2e/stratagems.cy.js
+++ cypress/e2e/stratagems.cy.js
@@ -1,104 +1,76 @@
 /* eslint-disable no-undef */
 describe('Stratagem Trainer - Gameplay and Settings', () => {
-  it('Validates gameplay flow and settings adjustments', () => {
+  beforeEach(() => {
+    // Centralize the visit and initial wait
     cy.visit('http://localhost:3000/');
+    cy.get('.container-is-opened').should('exist');
+  });
 
-    // wait for StrategemsLayout_opened__ybJp_ to be visible
-    cy.get('.StrategemsLayout_opened__ybJp_').should('exist');
+  it('Validates gameplay flow and settings adjustments', () => {
+    // Disable all stratagems
+    cy.toggleCheckbox('[data-testid="checkbox-all"]', false);
 
-    cy.toggleCheckbox(
-      '.StrategemsLayout_checkboxAll__ZLN7R > .Checkbox_checkbox-checkmark__kP1Gy',
-      false,
-    );
-
-    // Check msg when no stratagem is selected
-    cy.get('.StratagemsName_name__E4XEJ').should(
-      'have.text',
-      'Traitor detected !',
-    );
-    cy.get('.RoundInfo_round__WYuUI').should('have.text', '1');
+    // Check the message and round information
+    cy.get('[data-testid="stratagem-name"]').should('have.text', 'Traitor detected !');
+    cy.get('[data-testid="round-info"]').should('have.text', '1');
 
     // Select a stratagem
     cy.selectStratagem('Machine Gun');
 
-    // Perform a key combination
+    // Perform a correct combination
     cy.performKeyCombination(['down', 'left', 'down', 'up', 'right'], 6);
 
-    // Check if round is incremented
-    cy.get('.RoundInfo_round__WYuUI').should('have.text', '2');
-    cy.get(':nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ').should(
-      'have.text',
-      '75',
-    );
-    cy.get(':nth-child(3) > .ScoreInfo_bonusValue__ZTzmJ').should(
-      'have.text',
-      '100',
-    );
+    // Check if the round is incremented
+    cy.get('[data-testid="round-info"]').should('have.text', '2');
+    cy.get('[data-testid="round-bonus"]').should('have.text', '75');
+    cy.get('[data-testid="perfect-round"]').should('have.text', '100');
 
-    // Change sound settings
-    cy.get('.StratagemsGame_buttonSettings__JIFMu').click();
-    cy.get('#gameSound').check();
-    cy.get(':nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn').click();
+    // Modify sound settings
+    cy.get('[data-testid="button-settings"]').click();
+    cy.get('[data-testid="checkbox-game-sound"]').check();
+    cy.get('[data-testid="save-game-sound"]').click();
 
-    // Change timer settings
-    cy.get('#timerDuration').clear();
-    cy.get('#timerDuration').type('5');
-    cy.get(':nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn').click();
+    // Modify timer settings
+    cy.get('[data-testid="timer-duration-input"]').clear().type('5');
+    cy.get('[data-testid="save-timer-duration"]').click();
 
-    // click to close the settings
-    cy.get('.StratagemsGame_settingsOverlay__RuniU').click();
+    // Close settings
+    cy.get('[data-testid="settings-overlay"]').click();
 
-    // Check if game is loose
-    cy.get(':nth-child(1) > .ScoreInfo_bonusValue__ZTzmJ').should(
-      'have.text',
-      '0',
-    );
+    // Check if the game is lost
+    cy.get('[data-testid="round-bonus"]').should('have.text', '0');
   });
 
   it('Persists sound and timer settings after reload', () => {
     cy.setSettings(5);
     cy.visit('http://localhost:3000/');
-    cy.get('.StratagemsGame_buttonSettings__JIFMu').click();
-    cy.get('#timerDuration').should('have.value', '5');
+    cy.get('[data-testid="button-settings"]').click();
+    cy.get('[data-testid="timer-duration-input"]').should('have.value', '5');
   });
 
   it('Allows selecting and displaying stratagems', () => {
-    cy.visit('http://localhost:3000/');
-
-    // wait for StrategemsLayout_opened__ybJp_ to be visible
-    cy.get('.StrategemsLayout_opened__ybJp_').should('exist');
-
-    cy.toggleCheckbox(
-      '.StrategemsLayout_checkboxAll__ZLN7R > .Checkbox_checkbox-checkmark__kP1Gy',
-      false,
-    );
+    // Disable all stratagems
+    cy.toggleCheckbox('[data-testid="checkbox-all"]', false);
 
     // Select a stratagem
     cy.selectStratagem('Machine Gun');
-    cy.get('.StratagemsCard_active__rDaiJ').should('exist');
-    cy.get('.StratagemsCard_active__rDaiJ').should('have.length', 1);
+    cy.get('.card-is-active').should('exist').and('have.length', 1);
   });
 
   it('Handles successful and failed combinations', () => {
-    cy.visit('http://localhost:3000/');
-
-    // wait for StrategemsLayout_opened__ybJp_ to be visible
-    cy.get('.StrategemsLayout_opened__ybJp_').should('exist');
-
-    cy.toggleCheckbox(
-      '.StrategemsLayout_checkboxAll__ZLN7R > .Checkbox_checkbox-checkmark__kP1Gy',
-      false,
-    );
+    // Disable all stratagems
+    cy.toggleCheckbox('[data-testid="checkbox-all"]', false);
 
     // Select a stratagem
     cy.selectStratagem('Machine Gun');
 
     // Perform a correct combination
     cy.performKeyCombination(['down', 'left', 'down', 'up', 'right'], 1);
-    cy.get('.ScoreInfo_score__xU7Z2').should('have.text', '25');
+    cy.get('[data-testid="score"]').should('have.text', '25');
 
-    // Make a mistake in the combination
+    // Perform an incorrect combination
     cy.performKeyCombination(['down', 'left', 'up', 'up', 'right'], 1);
-    cy.get('.ScoreInfo_score__xU7Z2').should('have.text', '25');
+    cy.get('[data-testid="score"]').should('have.text', '25');
+    cy.get('.arrow-is-active').should('not.exist');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -16,12 +16,11 @@ Cypress.Commands.add('toggleCheckbox', (selector, shouldBeChecked) => {
  */
 Cypress.Commands.add('selectStratagem', (stratagemName) => {
   cy.get(
-    `[for="${stratagemName}"] > .StratagemsCard_card__sdnRd > .StratagemsCard_icon__bngXX`,
+    `[data-testid="stratagem-icon-${stratagemName}"]`,
   ).click();
   cy.get(`[id="${stratagemName}"]`).check({ force: true });
-  cy.get(
-    '.StratagemsGameCard_active__UNhvJ > .StratagemsGameCard_icon__yPyqA',
-  ).should('have.attr', 'alt', stratagemName);
+  cy.get(`[data-testid="stratagem-icon-${stratagemName}"]`)
+    .should('have.attr', 'alt', stratagemName);
 });
 
 /**
@@ -32,7 +31,7 @@ Cypress.Commands.add('selectStratagem', (stratagemName) => {
 Cypress.Commands.add('performKeyCombination', (keys, times = 1) => {
   for (let i = 0; i < times; i += 1) {
     keys.forEach((key) => {
-      cy.get('body').click().type(`{${key}arrow}`).wait(100);
+      cy.get('body').click().type(`{${key}arrow}`).wait(50);
     });
   }
 });
@@ -42,14 +41,14 @@ Cypress.Commands.add('setSettings', (timerDuration) => {
   cy.session('h2-settings', () => {
     cy.visit('http://localhost:3000/');
     // Change game sound settings
-    cy.get('.StratagemsGame_buttonSettings__JIFMu').click();
+    cy.get('[data-testid="button-settings"]').click();
     cy.wait(500);
-    cy.toggleCheckbox('#gameSound', true);
-    cy.get(':nth-child(1) > .Form_form__OfmeG > .Button_button__yboYn').click();
+    cy.toggleCheckbox('[data-testid="checkbox-game-sound"', true);
+    cy.get('[data-testid="save-game-sound"]').click();
 
     // Change timer settings
-    cy.get('#timerDuration').clear().type(timerDuration);
-    cy.get(':nth-child(2) > .Form_form__OfmeG > .Button_button__yboYn').click();
+    cy.get('[data-testid="timer-duration-input"]').clear().type(timerDuration);
+    cy.get('[data-testid="save-timer-duration"]').click();
     cy.wait(500);
   });
 });
