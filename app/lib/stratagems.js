8926f2fce7e5b590c837d3a531c570259a4b5763
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2025-01-08 19:36:08 +0100

    Enhance findDiffArray function to return differences from both arrays

--- app/lib/stratagems.js
+++ app/lib/stratagems.js
@@ -5,9 +5,12 @@
  * @param {Function} compareFn - A function to compare two items (should return true if equal).
  * @returns {Array} Items in arr1 that are not in arr2.
  */
-const findDiffArray = (arr1, arr2, compareFn) => arr1.filter((item1) => !arr2.some(
-  (item2) => compareFn(item1, item2),
-));
+const findDiffArray = (arr1, arr2, compareFn) => {
+  const diffFromArr1 = arr1.filter((item1) => !arr2.some((item2) => compareFn(item1, item2)));
+  const diffFromArr2 = arr2.filter((item2) => !arr1.some((item1) => compareFn(item1, item2)));
+
+  return [...diffFromArr1, ...diffFromArr2];
+};
 
 /**
  * Compares two stratagem objects by their unique identifier (e.g., `name` or `short_id`).
