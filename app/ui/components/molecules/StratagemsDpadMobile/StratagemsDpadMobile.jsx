c010979c1e6dfe2baf9cc82fe62df2045ea80261
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2025-02-27 18:03:50 +0100

    Add mobile D-pad component for Stratagems input

---
+++ app/ui/components/molecules/StratagemsDpadMobile/StratagemsDpadMobile.jsx
@@ -0,0 +1,58 @@
+// Styles
+import styles from './StratagemsDpadMobile.module.css';
+
+// Lib
+import cn from '../../../../lib/cn';
+
+// Components
+import { Picto } from '../../atoms/Picto/Picto';
+
+/**
+ * StratagemsDpadMobile component
+ * @returns {JSX.Element} The StratagemsDpadMobile component
+ */
+function StratagemsDpadMobile() {
+  return (
+    <div className={styles.dpadContainer}>
+      <StratagemsDpadMobile.Button keyName="up" keydownEvent={{ code: 'ArrowUp' }}>
+        <Picto icon="arrow" className={cn([styles.icon, styles.up])} />
+      </StratagemsDpadMobile.Button>
+      <StratagemsDpadMobile.Button keyName="left" keydownEvent={{ code: 'ArrowLeft' }}>
+        <Picto icon="arrow" className={cn([styles.icon, styles.left])} />
+      </StratagemsDpadMobile.Button>
+      <StratagemsDpadMobile.Button keyName="down" keydownEvent={{ code: 'ArrowDown' }}>
+        <Picto icon="arrow" className={cn([styles.icon, styles.down])} />
+      </StratagemsDpadMobile.Button>
+      <StratagemsDpadMobile.Button keyName="right" keydownEvent={{ code: 'ArrowRight' }}>
+        <Picto icon="arrow" className={cn([styles.icon, styles.right])} />
+      </StratagemsDpadMobile.Button>
+    </div>
+  );
+}
+
+/**
+ * StratagemsDpadMobileButton component
+ * @param {object} props - Component properties
+ * @param {object} props.keydownEvent - Keydown event
+ * @param {string} props.keyName - Key name
+ * @param {React.ReactNode} props.children - Child nodes
+ * @returns {JSX.Element} The StratagemsDpadMobileButton component
+ */
+StratagemsDpadMobile.Button = function StratagemsDpadMobileButton(
+  { keydownEvent, keyName, children },
+) {
+  return (
+    <button
+      type="button"
+      className={cn([styles.button, styles[`key-${keyName}`]])}
+      onClick={() => {
+        const event = new KeyboardEvent('keydown', { ...keydownEvent });
+        window.dispatchEvent(event);
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default StratagemsDpadMobile;
