5ccee49f0c2bdf669724bec419dace1ee77f71ec
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2025-03-01 18:15:04 +0100

    Add data-testid attributes to StratagemsDpadMobile buttons

--- app/ui/components/molecules/StratagemsDpadMobile/StratagemsDpadMobile.jsx
+++ app/ui/components/molecules/StratagemsDpadMobile/StratagemsDpadMobile.jsx
@@ -14,16 +14,32 @@ import { Picto } from '../../atoms/Picto/Picto';
 function StratagemsDpadMobile() {
   return (
     <div className={styles.dpadContainer}>
-      <StratagemsDpadMobile.Button keyName="up" keydownEvent={{ code: 'ArrowUp' }}>
+      <StratagemsDpadMobile.Button
+        keyName="up"
+        keydownEvent={{ code: 'ArrowUp' }}
+        data-testid="dpad-up"
+      >
         <Picto icon="arrow" className={cn([styles.icon, styles.up])} />
       </StratagemsDpadMobile.Button>
-      <StratagemsDpadMobile.Button keyName="left" keydownEvent={{ code: 'ArrowLeft' }}>
+      <StratagemsDpadMobile.Button
+        keyName="left"
+        keydownEvent={{ code: 'ArrowLeft' }}
+        data-testid="dpad-left"
+      >
         <Picto icon="arrow" className={cn([styles.icon, styles.left])} />
       </StratagemsDpadMobile.Button>
-      <StratagemsDpadMobile.Button keyName="down" keydownEvent={{ code: 'ArrowDown' }}>
+      <StratagemsDpadMobile.Button
+        keyName="down"
+        keydownEvent={{ code: 'ArrowDown' }}
+        data-testid="dpad-down"
+      >
         <Picto icon="arrow" className={cn([styles.icon, styles.down])} />
       </StratagemsDpadMobile.Button>
-      <StratagemsDpadMobile.Button keyName="right" keydownEvent={{ code: 'ArrowRight' }}>
+      <StratagemsDpadMobile.Button
+        keyName="right"
+        keydownEvent={{ code: 'ArrowRight' }}
+        data-testid="dpad-right"
+      >
         <Picto icon="arrow" className={cn([styles.icon, styles.right])} />
       </StratagemsDpadMobile.Button>
     </div>
