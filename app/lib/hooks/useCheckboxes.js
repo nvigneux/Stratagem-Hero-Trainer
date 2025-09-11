d44f73ba3ebc59f0c27a87d808b5c7f6cc4d5606
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2025-09-11 20:59:22 +0200

    Enhance useCheckboxes hook to manage checkbox state with order
    - Added a new function to retrieve the highest order from checkboxes.
    - Updated checkbox state management to include order alongside value.
    - Modified handleChange to set the order for newly checked checkboxes.
    - Ensured consistent structure for checkbox state updates.

--- app/lib/hooks/useCheckboxes.js
+++ app/lib/hooks/useCheckboxes.js
@@ -3,6 +3,19 @@
 import { useState } from 'react';
 
 /**
+ * Get the highest order found in checkboxes
+ * @param {{ [key: string]: { value: boolean, order: number } }} checkboxes - Checkboxes object
+ * @returns {number} The maximum order found
+ */
+function getMaxOrderFromCheckboxes(checkboxes) {
+  const orderedCheckboxes = Math.max(0, ...Object.values(checkboxes)
+    .map((checkbox) => checkbox.order)
+    .sort((a, b) => b - a));
+
+  return orderedCheckboxes;
+}
+
+/**
  * Custom hook for managing checkboxes state.
  * @param {object} params - Parameters.
  * @param {Array} params.initialState - Initial state of the checkboxes.
@@ -11,25 +24,37 @@ import { useState } from 'react';
  * @returns {object} Checkboxes state and control functions.
  */
 function useCheckboxes({ initialState, key, defaultValue }) {
-  const [checkboxes, setCheckboxes] = useState(initialState.reduce((acc, item) => {
-    acc[item[key]] = defaultValue;
+  /** @type {[{ [key: string]: { value: boolean, order: number } }, React.Dispatch<React.SetStateAction<{ [key: string]: { value: boolean, order: number } }>>]} */
+  const [
+    checkboxes,
+    setCheckboxes,
+  ] = useState(initialState.reduce((acc, item) => {
+    acc[item[key]] = { value: defaultValue, order: 0 };
     return acc;
   }, {}));
 
-  const checkboxesAreChecked = Object.values(checkboxes).every((value) => value);
+  const checkboxesAreChecked = Object.values(checkboxes).every((value) => value.value);
 
   /**
    * Handle the change of multiple checkboxes
-   * @param {{[key: string]: boolean}} keyValues - Key-value pairs to update
+   * @param {{[key: string]: { value: boolean, order: number }}} keyValues - Key-value pairs to update
    */
   const handleChange = (keyValues) => {
+    const maxOrderFound = getMaxOrderFromCheckboxes(checkboxes);
+    const lastOrder = maxOrderFound + 1;
+
     setCheckboxes((prevState) => ({
       ...prevState,
       ...Object.keys(keyValues).reduce((
         acc,
         name,
       ) => {
-        acc[name] = keyValues[name] !== undefined ? keyValues[name] : !prevState[name];
+        acc[name] = keyValues[name] !== undefined
+          ? keyValues[name]
+          : {
+            value: !prevState[name].value,
+            order: lastOrder,
+          };
         return acc;
       }, {}),
     }));
@@ -42,7 +67,10 @@ function useCheckboxes({ initialState, key, defaultValue }) {
     setCheckboxes((prevState) => {
       const newState = {};
       Object.keys(prevState).forEach((keyValue) => {
-        newState[keyValue] = !checkboxesAreChecked;
+        newState[keyValue] = {
+          value: !checkboxesAreChecked,
+          order: 0,
+        };
       });
       return newState;
     });
